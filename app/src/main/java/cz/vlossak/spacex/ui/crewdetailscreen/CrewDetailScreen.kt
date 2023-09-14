e47f24a60c910107cd6b9c1f535ae384adac0c5a
DESKTOP-V6M5IEN\Jakub <jakub.vlossak@email.cz>
2023-09-14 22:38:29 +0200

    CrewDetailScreen UI update

--- app/src/main/java/cz/vlossak/spacex/ui/crewdetailscreen/CrewDetailScreen.kt
+++ app/src/main/java/cz/vlossak/spacex/ui/crewdetailscreen/CrewDetailScreen.kt
@@ -1,18 +1,20 @@
 package cz.vlossak.spacex.ui.crewdetailscreen
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import cz.vlossak.spacex.R
 import androidx.compose.ui.res.stringResource
@@ -47,35 +49,38 @@ private fun DetailScreen(viewState: CrewDetailViewState) {
             .padding(10.dp, 60.dp, 10.dp, 10.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        AsyncImage(
-            model = viewState.data.image,
-            contentDescription = "Image",
-            contentScale = ContentScale.Fit
-        )
-        Spacer(modifier = Modifier.height(10.dp))
-        Text(text = viewState.data.name, style = Typography.headlineMedium)
-        Row(modifier = Modifier.padding(start = 20.dp, end = 20.dp)) {
-            Column(horizontalAlignment = Alignment.Start) {
-                Text(
-                    text = stringResource(id = R.string.Agency),
-                    style = Typography.bodyMedium
-                )
-                Text(
-                    text = stringResource(id = R.string.Status),
-                    style = Typography.bodyMedium
-                )
-            }
-            Spacer(modifier = Modifier.width(20.dp))
-            Column(horizontalAlignment = Alignment.End) {
-                Text(
-                    text = viewState.data.agency,
-                    style = Typography.bodyMedium
-                )
-                Text(
-                    text = viewState.data.status,
-                    style = Typography.bodyMedium
-                )
-            }
+        Column(
+            modifier = Modifier.weight(1f),
+            verticalArrangement = Arrangement.Center
+        ) {
+            AsyncImage(
+                modifier = Modifier.clip(shape = RoundedCornerShape(25.dp)),
+                model = viewState.data.image,
+                contentDescription = "Image",
+                contentScale = ContentScale.Fit
+            )
         }
+        Column(modifier = Modifier.weight(1f)) {
+            Text(text = viewState.data.name, style = Typography.headlineMedium)
+            DetailBlock(
+                text = viewState.data.agency,
+                string = R.string.Agency
+            )
+            DetailBlock(
+                text = viewState.data.status,
+                string = R.string.Status
+            )
+        }
+
+    }
+}
+
+@Composable
+private fun DetailBlock(text: String, string: Int) {
+    Column(modifier = Modifier.padding(5.dp)) {
+        Divider()
+        Spacer(modifier = Modifier.height(5.dp))
+        Text(text = stringResource(id = string), style = Typography.bodySmall)
+        Text(text = text, style = Typography.bodyMedium)
     }
 }
